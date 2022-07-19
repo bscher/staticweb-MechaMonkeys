3f2e58faa1501ae0e196a871f09c37c2aaf8841e
Blake Scherschel <blake.scherschel@gmail.com>
2022-07-19 15:53:53 -0700

    Final edition for post-mint, but before any added details.

--- src/scene_mint/Station.js
+++ src/scene_mint/Station.js
@@ -29,120 +29,122 @@ class Station {
         frame_justPlatform: 14
     };
 
-    static image_cover = {
-        image: (() => {
-            const _img = new Image();
-            _img.src = "img/scene_mint/station_cover.png";
-            return _img;
-        })(),
-        frameWidth: 750,
-        frameHeight: 750,
-    };
+    // static image_cover = {
+    //     image: (() => {
+    //         const _img = new Image();
+    //         _img.src = "img/scene_mint/station_cover.png";
+    //         return _img;
+    //     })(),
+    //     frameWidth: 750,
+    //     frameHeight: 750,
+    // };
 
     constructor(canvas, ctx) {
         this.canvas = canvas;
         this.ctx = ctx;
 
         this.state = {
-            enabledAtUTCTime: 1657933200000,
-            mouseHoveringOver_mm: false,
-            mouseHoveringOver_gme: false,
-            isAssociated: false,
-            isGME: false,
-            isEth: false,
-            mouseHoveringOver_mint: false,
-            doneWithStation: false,
-            doneWithStationTimeStamp: 0
+            countdown1Timestamp: 1657933200000, // Friday, July 15, 2022 6:00:00 PM GMT-07:00
+            countdown1FinishedTimestamp: 0,
+            countdownIntermissionMillis: 10000,
+            countdownIntermissionStarted: false,
+            countdownIntermissionEnded: false,
+            countdown2Timestamp: 1659142800000, // Friday, July 29, 2022 6:00:00 PM GMT-07:00
+            countdown2FinishedTimestamp: 0,
+            //mouseHoveringOver_mm: false,
+            //mouseHoveringOver_gme: false,
+            //isAssociated: false,
+            //isGME: false,
+            //isEth: false,
+            //mouseHoveringOver_mint: false,
+            //doneWithStation: false,
+            //doneWithStationTimeStamp: 0
         };
 
+        this.TIME_DIFF = null;
+
+        // Skip countdown #1 if after time
+        const utcNow = (new Date()).getTime();
+        if (utcNow > this.state.countdown1Timestamp) {
+            this.state.countdownIntermissionStarted = true;
+            this.state.countdownIntermissionEnded = true;
+        }
+
         this.clickregion_associate_mm = document.getElementById('screen_clickregion_associate_mm');
-        (() => {
-            this.clickregion_associate_mm.hidden = true;
-            this.clickregion_associate_mm.addEventListener('mouseover', (event) => { this.state.mouseHoveringOver_mm = true; });
-            this.clickregion_associate_mm.addEventListener('mouseleave', (event) => { this.state.mouseHoveringOver_mm = false; });
-            this.clickregion_associate_mm.addEventListener('click', (event) => {
-                console.log("Clicked associate MM!");
-                this.attemptToConnectToEthWallet();
-            });
-        })();
+        // (() => {
+        //     this.clickregion_associate_mm.addEventListener('mouseover', (event) => { this.state.mouseHoveringOver_mm = true; });
+        //     this.clickregion_associate_mm.addEventListener('mouseleave', (event) => { this.state.mouseHoveringOver_mm = false; });
+        //     this.clickregion_associate_mm.addEventListener('click', (event) => {
+        //         console.log("Clicked associate MM!");
+        //         this.attemptToConnectToEthWallet();
+        //     });
+        // })();
         this.clickregion_associate_gme = document.getElementById('screen_clickregion_associate_gme');
-        (() => {
-            this.clickregion_associate_gme.hidden = true;
-            this.clickregion_associate_gme.addEventListener('mouseover', (event) => { this.state.mouseHoveringOver_gme = true; });
-            this.clickregion_associate_gme.addEventListener('mouseleave', (event) => { this.state.mouseHoveringOver_gme = false; });
-            this.clickregion_associate_gme.addEventListener('click', (event) => {
-                console.log("Clicked associate GME!");
-                this.attemptToConnectToGMEWallet();
-            });
-        })();
+        // (() => {
+        //     this.clickregion_associate_gme.addEventListener('mouseover', (event) => { this.state.mouseHoveringOver_gme = true; });
+        //     this.clickregion_associate_gme.addEventListener('mouseleave', (event) => { this.state.mouseHoveringOver_gme = false; });
+        //     this.clickregion_associate_gme.addEventListener('click', (event) => {
+        //         console.log("Clicked associate GME!");
+        //         this.attemptToConnectToGMEWallet();
+        //     });
+        // })();
         this.clickregion_mint = document.getElementById('screen_clickregion_mint');
-        (() => {
-            this.clickregion_mint.hidden = true;
-            this.clickregion_mint.addEventListener('mouseover', (event) => { this.state.mouseHoveringOver_mint = true; });
-            this.clickregion_mint.addEventListener('mouseleave', (event) => { this.state.mouseHoveringOver_mint = false; });
-            this.clickregion_mint.addEventListener('click', (event) => {
-                console.log("Clicked mint!");
-                if (this.state.isAssociated) {
-                    this.attemptToMint()
-                        .then((success) => {
-                            console.log("Tried to mint. Returned non-error.");
-                            if (success) {
-                                this.startYoinkAnimation((new Date()).getTime());
-                            }
-                        })
-                        .catch((error) => {
-                            console.log("Failed to mint.");
-                            console.log(error);
-                        });
-                }
-            });
-        })();
+        // (() => {
+        //     this.clickregion_mint.addEventListener('mouseover', (event) => { this.state.mouseHoveringOver_mint = true; });
+        //     this.clickregion_mint.addEventListener('mouseleave', (event) => { this.state.mouseHoveringOver_mint = false; });
+        //     this.clickregion_mint.addEventListener('click', (event) => {
+        //         console.log("Clicked mint!");
+        //         if (this.state.isAssociated) {
+        //             this.attemptToMint()
+        //                 .then((success) => {
+        //                     console.log("Tried to mint. Returned non-error.");
+        //                 })
+        //                 .catch((error) => {
+        //                     console.log("Failed to mint.");
+        //                     console.log(error);
+        //                 });
+        //         }
+        //     });
+        // })();
 
         this.toggleAllButtons(false);
     }
 
-    attemptToConnectToEthWallet() {
-        connectToEthWallet()
-            .then((connectSuccess) => {
-                console.log("Connect returned: " + connectSuccess);
-                if (connectSuccess) {
-                    this.state.isAssociated = true;
-                    this.state.isEth = true;
-                }
-            })
-            .catch((err) => {
-                console.log("Failed to connect to Eth wallet: " + err);
-            });
-    }
+    // attemptToConnectToEthWallet() {
+    //     connectToEthWallet()
+    //         .then((connectSuccess) => {
+    //             console.log("Connect returned: " + connectSuccess);
+    //             if (connectSuccess) {
+    //                 this.state.isAssociated = true;
+    //                 this.state.isEth = true;
+    //             }
+    //         })
+    //         .catch((err) => {
+    //             console.log("Failed to connect to Eth wallet: " + err);
+    //         });
+    // }
 
-    attemptToConnectToGMEWallet() {
-        connectToGMEWallet()
-            .then((connectSuccess) => {
-                console.log("Connect returned: " + connectSuccess);
-                if (connectSuccess) {
-                    this.state.isAssociated = true;
-                    this.state.isGME = true;
-                }
-            })
-            .catch((err) => {
-                console.log("Failed to connect to GME wallet: " + err);
-            });
-    }
+    // attemptToConnectToGMEWallet() {
+    //     connectToGMEWallet()
+    //         .then((connectSuccess) => {
+    //             console.log("Connect returned: " + connectSuccess);
+    //             if (connectSuccess) {
+    //                 this.state.isAssociated = true;
+    //                 this.state.isGME = true;
+    //             }
+    //         })
+    //         .catch((err) => {
+    //             console.log("Failed to connect to GME wallet: " + err);
+    //         });
+    // }
 
-
-    async attemptToMint() {
-        // if (this.state.isEth) {
-        //     return await attemptEthMint();
-        // } else {
-        //     return await attemptGMEMint();
-        // }
-    }
-
-    startYoinkAnimation(timeStamp) {
-        // this.state.doneWithStation = true;
-        // this.state.doneWithStationTimeStamp = timeStamp;
-        // this.toggleAllButtons(false);
-    }
+    // async attemptToMint() {
+    //     if (this.state.isEth) {
+    //         return await attemptEthMint();
+    //     } else {
+    //         return await attemptGMEMint();
+    //     }
+    // }
 
     toggleMintButton(isButtonEnabled) {
         this.clickregion_mint.hidden = !isButtonEnabled;
@@ -158,77 +160,71 @@ class Station {
         this.toggleMintButton(areButtonsEnabled);
     }
 
-    drawTimer(timeStamp, totalMillisRemainingUntilMint) {
-        if (totalMillisRemainingUntilMint > 0) {
-            const millis_str = getNumberAsFixedString(totalMillisRemainingUntilMint % 1000, 3);
-            const secs_str = getNumberAsFixedString(Math.floor(totalMillisRemainingUntilMint / 1000) % 60, 2);
-            const mins_str = getNumberAsFixedString(Math.floor(totalMillisRemainingUntilMint / 1000 / 60) % 60, 2);
-            const hours_str = getNumberAsFixedString(Math.floor(totalMillisRemainingUntilMint / 1000 / 60 / 60) % 60, 2);
-            const days_str = getNumberAsFixedString(Math.min(99, Math.floor(totalMillisRemainingUntilMint / 1000 / 60 / 60 / 24)), 3);
+    drawMechs() {
 
+    }
+
+    drawTimer(timeStamp, totalMillisRemaining, textOverride) {
+        let textToRender = textOverride;
+        if (!textToRender) {
+            const millis_str = getNumberAsFixedString(totalMillisRemaining % 1000, 3);
+            const secs_str = getNumberAsFixedString(Math.floor(totalMillisRemaining / 1000) % 60, 2);
+            const mins_str = getNumberAsFixedString(Math.floor(totalMillisRemaining / 1000 / 60) % 60, 2);
+            const hours_str = getNumberAsFixedString(Math.floor(totalMillisRemaining / 1000 / 60 / 60) % 60, 2);
+            const days_str = getNumberAsFixedString(Math.min(99, Math.floor(totalMillisRemaining / 1000 / 60 / 60 / 24)), 3);
+
+            textToRender = `${days_str}:${hours_str}:${mins_str}:${secs_str}.${millis_str}`;
+        }
+        if (textOverride && ((timeStamp % 1500) > 750)) {
+            // Render nothing
+        } else {
             this.ctx.save();
             this.ctx.fillStyle = 'rgba(255,0,0,1)';
-            this.ctx.font = "bold 26px monospace";
-            this.ctx.fillText(`${days_str}:${hours_str}:${mins_str}:${secs_str}.${millis_str}`, 265, 653);
+            this.ctx.font = "bold 26px courier";
+            this.ctx.fillText(textToRender, 253, 652);
             this.ctx.restore();
-        } else {
-            this.toggleAllButtons(true);
-            if (timeStamp % 1500 > 750) {
-                this.ctx.save();
-                this.ctx.fillStyle = 'rgba(255,0,0,1)';
-                this.ctx.font = "bold 26px monospace";
-                this.ctx.fillText(`UPDATE IN PROGRESS`, 265, 653);
-                this.ctx.restore();
-            }
         }
     }
 
     draw(timeStamp) {
-        const totalMillisRemainingUntilMint = Math.max(0, this.state.enabledAtUTCTime - timeStamp);
+        if (this.TIME_DIFF) {
+            timeStamp += this.TIME_DIFF;
+        }
 
-        let frame = Station.animation.frame_justPlatform;
-        // if (totalMillisRemainingUntilMint === 0) {
-        //     if (this.state.isAssociated) {
-        //         if (this.state.doneWithStation) {
-        //             const elapsedTime = timeStamp - this.state.doneWithStationTimeStamp;
-        //             const maxAnimationTime = Station.animation.frameCount * Station.animation.framePeriod;
-        //             if (elapsedTime > maxAnimationTime) {
-        //                 frame = Station.animation.frame_justPlatform;
-        //             } else {
-        //                 frame = getFrameFromTime(elapsedTime, Station.animation.frameCount, Station.animation.framePeriod);
-        //             }
-        //         } else {
-        //             frame = Station.animation.frame_ggg;
-        //         }
-        //     } else {
-        //         if (this.state.mouseHoveringOver_mm) {
-        //             frame = Station.animation.frame_grr;
-        //         } else if (this.state.mouseHoveringOver_gme) {
-        //             frame = Station.animation.frame_rgr;
-        //         } else {
-        //             frame = Station.animation.frame_rrr;
-        //         }
-        //     }
-        // }
         this.ctx.drawImage(
             Station.animation.image,
-            frame * Station.animation.frameWidth, 0,
+            Station.animation.frame_justPlatform * Station.animation.frameWidth, 0,
             Station.animation.frameWidth, Station.animation.frameHeight,
             0, 0,
             Station.animation.frameWidth, Station.animation.frameHeight
         );
 
-        // if (totalMillisRemainingUntilMint > 0) {
-        //     this.ctx.drawImage(
-        //         Station.image_cover.image,
-        //         0, 0,
-        //         Station.image_cover.frameWidth, Station.image_cover.frameHeight,
-        //         0, 0,
-        //         Station.image_cover.frameWidth, Station.image_cover.frameHeight
-        //     );
-        // }
+        if (!this.state.countdownIntermissionStarted && !this.state.countdownIntermissionEnded) {
+            // Render timer for #1
+            this.drawTimer(timeStamp, this.state.countdown1Timestamp - timeStamp, null);
 
-        this.drawTimer(timeStamp, totalMillisRemainingUntilMint);
+            if (timeStamp > this.state.countdown1Timestamp) {
+                this.state.countdown1FinishedTimestamp = timeStamp;
+                this.state.countdownIntermissionStarted = true;
+            }
+        } else if (this.state.countdownIntermissionStarted && !this.state.countdownIntermissionEnded) {
+            // Render text for revealed
+            this.drawTimer(timeStamp, 0, "-- REVEAL, GO --");
+            this.drawMechs();
+
+            if ((timeStamp - this.state.countdown1FinishedTimestamp) > this.state.countdownIntermissionMillis) {
+                this.state.countdownIntermissionEnded = true;
+            }
+        } else if (this.state.countdownIntermissionStarted && this.state.countdownIntermissionEnded) {
+            // Render timer for #2
+            if (timeStamp < this.state.countdown2Timestamp) {
+                this.drawTimer(timeStamp, this.state.countdown2Timestamp - timeStamp, null);
+            } else {
+                // Render text for completed
+                this.drawTimer(timeStamp, 0, "--  COMPLETE  --");
+            }
+            this.drawMechs();
+        }
     }
 }
 
