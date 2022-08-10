fe257d2566c29dea00b3029479d5c88a67a7559d
Blake Scherschel <blake.scherschel@gmail.com>
2022-08-10 14:22:46 -0700

    Updated strip_exif script to display progress as it is running.

--- strip_exif_image_data.sh
+++ strip_exif_image_data.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-for f in `find . -type f -not \( -path "*/node_modules/*" -o -path "*/openzeppelin-contracts/*" -o -path "*/build/*" \) \
-                              \( -iname \*.png -o -iname \*.jpg \)`; do
-    echo "$f"
-    mogrify -strip "$f"
+if ! command -v mogrify &> /dev/null; then
+    echo "Count not find 'mogrify' in PATH. Please install this tool and run again."
+    exit 1
+fi
+
+FILES_TO_PRUNE=`find . -type f -not \( -path "*/node_modules/*" -o -path "*/openzeppelin-contracts/*" -o -path "*/build/*" \) \
+                                    \( -iname \*.png -o -iname \*.jpg \)`
+NUM_FILES=`echo "$FILES_TO_PRUNE" | wc -l`
+
+file_counter=1
+for f in $FILES_TO_PRUNE; do
+    echo "Pruning [$file_counter/$NUM_FILES] $f"
+    #mogrify -strip "$f"
+    let "file_counter = file_counter + 1" 
 done
